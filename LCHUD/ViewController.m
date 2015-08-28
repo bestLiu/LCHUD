585333e64e511381aadcea45b83c105b692a80f3
mac1 <cyjjkz1@gmail.com>
2015-08-28 13:33:51 +0800

    显示普通文字

---
+++ LCHUD/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  LCHUD
+//
+//  Created by mac1 on 15/8/28.
+//  Copyright (c) 2015年 BNDK. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BNBlackHUD.h"
+
+@interface ViewController ()
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+- (IBAction)buttonClick:(id)sender
+{
+    [BNBlackHUD showMessage:@"我的天，好无聊啊~~~~" toView:self.view];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
